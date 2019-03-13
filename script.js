8f8a0faa8b9cb0b500fe72638617b28217de4d1d
Oliver Bikar <oliverbikar@yahoo.co.uk>
2019-03-13 16:03:35 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function maintenanceMessage() {
+	alert("Content for this system is currently undergoing maintenance and will be available soon");
+}
+
+function searchDevices() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('search');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("gaming-systems");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } 
+	else {
+      li[i].style.display = "none";
+	  //document.getElementById("devices").innerHTML = "No Matching Results Found";
+    }
+  }
+}
